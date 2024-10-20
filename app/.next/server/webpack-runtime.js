8246709da931a8886a9b5b47abe701a47fbc4f12
Humayun Kabir <mhkmcp@yahoo.com>
2024-10-21 01:44:23 +0200

    resolved style issue, added redux

--- app/.next/server/webpack-runtime.js
+++ app/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("bcf57eafa9b10824")
+/******/ 		__webpack_require__.h = () => ("25fda27824eb7a98")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
